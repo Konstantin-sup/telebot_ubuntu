c4b8e350871e4d709af356fca099f89920ae6aca
Konstantin-sup <konstantin.artemovich.yudin18.ua@gmail.com>
2026-04-12 20:07:54 +0200

    created func 'reaction_to_button' in main.py not finished yet

--- main_bot_package/main_bot.py
+++ main_bot_package/main_bot.py
@@ -19,6 +19,18 @@ def start(message):
         reply_markup=markup
     )
 
+@BOT.message_handler(func=lambda message:True)
+def reaction_to_button(message):
+    if message.text not in ["📁 My files", "📤 Upload",
+                            "🗑️ Delete", "🔎 Search", "❓ Help"]:
+        BOT.send_message(
+            message.chat.id,
+            "Sorry, currently are only those commands into use👇",
+            reply_markup=create_keyboard_panel()
+        )
+
+
+
 
 BOT.polling()
 
