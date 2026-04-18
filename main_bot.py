70da2eb4969e99f2a868395bbad1d3da848144dc
Konstantin-sup <konstantin.artemovich.yudin18.ua@gmail.com>
2026-04-18 19:45:22 +0200

    remake 'save_file' func so now files can be saved

--- main_bot.py
+++ main_bot.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 import telebot
 from main_bot_package.telebot_functions import create_keyboard_panel
-from main_bot_package.file_date_functions import save_text
+from main_bot_package.file_date_functions import save_file
 
 load_dotenv()  #loading .env
 
@@ -13,17 +13,23 @@ COMMANDS = ["📁 My files", "📤 Upload", "🗑️ Delete", "🔎 Search", "�
 
 def load_data(message):
     try:
-        if message.voice or message.photo:  #takes text only.
+        if message.content_type in ['voice', 'video_note', 'photo']:  #currently takes text, documents only.
             raise TypeError
 
         elif message.text:
-            save_text(message.from_user.id, text=message.text)
-            BOT.send_message(message.chat.id, "Text was saved successfully")
+            save_file(message.from_user.id, text=message.text)
+            BOT.send_message(message.chat.id, "Text was saved successfully✅")
 
-       # elif message.document:
+        elif message.document:
+            BOT.send_message(message.chat.id, "Got it, may take a lil time⌛ to save it")
+            file_id = message.document.file_id
+            file_info = BOT.get_file(file_id)
+            downloaded_bytes = BOT.download_file(file_info.file_path)
+            save_file(message.from_user.id, file_bytes=downloaded_bytes, file_name=message.document.file_name)
+            BOT.send_message(message.chat.id, "File was saved successfully✅")
 
     except TypeError:
-        BOT.send_message(message.chat.id, "Currently is only text allowed, try again")
+        BOT.send_message(message.chat.id, "Currently are only text and documents allowed, try again")
         BOT.register_next_step_handler(message, load_data)
 
 
--- main_bot_package/file_date_functions.py
+++ main_bot_package/file_date_functions.py
@@ -36,19 +36,19 @@ def get_time_data():  #returns always current time, needs on server where reques
 def file_count(dir_path):
     return len(os.listdir(dir_path))
 
-def save_text(us_id, text=None):
-    if text:
-        user_id = us_id  #better to make users_dir with theirs id(they are unique)
-        time = get_time_data()
 
-        user_dir = os.path.join(data_path, str(user_id))
-        os.makedirs(user_dir, exist_ok=True)  #makes dir for new user if not exists
-        os.makedirs(os.path.join(user_dir, time.get("month")), exist_ok=True) #makes new month dir in users_dir if not exists
+def save_file(us_id, text=None, file_bytes=None, file_name=None):
+    user_id = us_id  #better to make users_dir with theirs id(they are unique)
+    time = get_time_data()
+    user_dir = os.path.join(data_path, str(user_id))
+    os.makedirs(user_dir, exist_ok=True)  #makes dir for new user if not exists
+    os.makedirs(os.path.join(user_dir, time.get("month")), exist_ok=True) #makes new month dir in users_dir if not exists
+    path_current_date_dir = os.path.join(user_dir, time.get("month"), time.get("dir"))
+
+    if text:  #so now if user sends text to save as  more then once we create a dir for that
         new_f_path = os.path.join(user_dir, time.get("month"), time.get("filename"))  #path to the new file
-        path_current_date_dir = os.path.join(user_dir, time.get("month"), time.get("dir"))  #makes path to the dir, if it's needed in cases
 
-        #so now if user sends text to save as  more then once we create a dir for that
-        if os.path.exists(new_f_path):
+        if os.path.exists(new_f_path):  #checks if file is in root month dir
             os.makedirs(path_current_date_dir, exist_ok=True)
             shutil.move(new_f_path, path_current_date_dir)  #moves old file to new dir
             file_counted = file_count(path_current_date_dir)  #counting files in the dir
@@ -66,6 +66,11 @@ def save_text(us_id, text=None):
             with open(new_f_path, 'w', encoding='utf-8') as f_obj:
                 f_obj.write(text)
 
+    if file_bytes:
+        os.makedirs(path_current_date_dir, exist_ok=True)
+        os.chdir(path_current_date_dir)
+        with open(file_name, "wb") as f_obj:
+            f_obj.write(file_bytes)
 
 
 
