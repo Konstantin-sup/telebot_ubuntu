0405d55d1646ec8ddd157e7a7addb2232983eb9b
Konstantin-sup <konstantin.artemovich.yudin18.ua@gmail.com>
2026-04-19 20:37:42 +0200

    created alembic migration, and made db table for file metadeta

---
+++ db_model/main_table_model.py
@@ -0,0 +1,14 @@
+from db_model.declarative_base import Base
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import VARCHAR, Integer
+
+class MainTable(Base):
+    __tablename__ = "main_table"
+
+    user_id: Mapped[str] = mapped_column(VARCHAR(60), primary_key=True)
+    file_id: Mapped[str] = mapped_column(VARCHAR(255), unique=True)
+    file_path: Mapped[str] = mapped_column(VARCHAR(255))
+
+
+
+    
