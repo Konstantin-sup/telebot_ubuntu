c55d880c33befd4da42782fe4c6d14c0e99342b7
Konstantin-sup <konstantin.artemovich.yudin18.ua@gmail.com>
2026-04-28 20:29:33 +0200

    created new colums date and month_dir

--- db_model/main_table_model.py
+++ db_model/main_table_model.py
@@ -1,8 +1,8 @@
 import os
 from db_model.declarative_base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import VARCHAR
-from sqlalchemy import create_engine
+from sqlalchemy import VARCHAR, create_engine, DateTime
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()  #loading .env
@@ -16,6 +16,8 @@ class MainTable(Base):
     user_id: Mapped[str] = mapped_column(VARCHAR(60))
     file_id: Mapped[str] = mapped_column(VARCHAR(255), primary_key=True)
     file_path: Mapped[str] = mapped_column(VARCHAR(255), unique=True)
+    date_creation: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    month_dir: Mapped[str] = mapped_column(VARCHAR(60))
     tele_file_id: Mapped [str] = mapped_column(VARCHAR(255))
 
 
